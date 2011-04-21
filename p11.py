f1d55845ee61b104c76adf5e197b81d7bb5e6b84
Tim Song <timsong@uvic.ca>
2011-04-21 13:34:22 -0700

    Solved Problem 11

---
+++ p11.py
@@ -0,0 +1,20 @@
+grid = [ [int(n) for n in l.strip().split()] for l in open('p11.in') ]
+D = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]
+
+m = 0 
+for i in xrange(len(grid)):
+	for j in xrange(len(grid[i])):
+		for (dx,dy) in D:
+			p = 1
+			for k in xrange(4):
+				x = i+k*dx
+				y = j+k*dy
+				if x >= 0 and x < len(grid) and y >= 0 and y < len(grid[i]):
+					p *= grid[x][y]
+
+			m = max(m, p)
+
+print m
+
+
+
