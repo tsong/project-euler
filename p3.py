fd190d61c76768e272254fe770df2718f457bb5f
Tim Song <timsong@uvic.ca>
2011-04-23 21:52:56 -0700

    Fixed problem 3 to use eulerlib factor

--- p3.py
+++ p3.py
@@ -1,19 +1,3 @@
-def isprime(n):
-	i = 2
-	while i < n:
-		if n % i == 0:
-			return False
-		i += 1
-	return True
-
-i = 2
-n = 600851475143
-m = -1
-while n > 1: 
-	if n % i == 0 and isprime(i):
-		n /= i
-		m = max(m,i)
-	i += 1
-
-print m
-
+from eulerlib import factor
+factors = factor(600851475143)
+print factors[len(factors)-1]
